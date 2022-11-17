e67ae581d4a9dc7c051bc2902338cd3dbc48da1f
ZakYeo <zakyeomanson@gmail.com>
2022-11-17 15:32:17 +0000

    Removed unused imports

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import addDummyData from './addDummyData'
 import MainNavigationStack from './app/stacks/MainNavigationStack';
-
-import localData from './app/assets/all-places.json'
 
 /**
    * This app is a redesign of the National Trust's current app from the store.
